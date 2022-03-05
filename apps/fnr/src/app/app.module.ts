54f66b11bed9a0c9192711d394f30ae3ca94f6ca
Lee Khai <leekhai18@gmail.com>
2022-03-06 00:55:12 +0700

    initial commit

---
+++ apps/fnr/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {NZ_I18N, vi_VN} from 'ng-zorro-antd/i18n';
+import {registerLocaleData} from '@angular/common';
+import vi from '@angular/common/locales/vi';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {IconsProviderModule} from './icons-provider.module';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+import {NzMenuModule} from 'ng-zorro-antd/menu';
+
+registerLocaleData(vi);
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, IconsProviderModule, NzLayoutModule, NzMenuModule],
+  providers: [{provide: NZ_I18N, useValue: vi_VN}],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
